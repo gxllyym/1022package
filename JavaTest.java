65dae0c3c7c79366b21134f60e3bd85142b6852a
杨玉蒙 <34531830+gxllyym@users.noreply.github.com>
2019-10-22 11:01:06 +0800

    Update JavaTest.java

--- JavaTest.java
+++ JavaTest.java
@@ -1,2 +1,23 @@
 package Test2_Junit4;
 
+import junit.framework.*;
+//继承TestCase类
+public class JavaTest extends TestCase {
+   protected int value1, value2;
+
+   // assigning the values
+   protected void setUp(){
+      value1=9;
+      value2=3;
+   }
+
+   // test method to add two values
+   public void testAdd(){
+      double result= value1 + value2;
+      assertTrue(result == 12);
+   }
+   public void testSub(){
+	      double result= value1 - value2;
+	      assertTrue(result == 6);
+	   }
+}
